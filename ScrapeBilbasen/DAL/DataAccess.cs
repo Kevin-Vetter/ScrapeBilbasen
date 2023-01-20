eec26cf5444e5d61f05140fddad9c657a1e865ff
Bjarke <bjarkeholmehan@gmail.com>
2023-01-20 23:09:59 +0100

    Set up DataAccess class

--- ScrapeBilbasen/DAL/DataAccess.cs
+++ ScrapeBilbasen/DAL/DataAccess.cs
@@ -1,6 +1,30 @@
-﻿namespace ScrapeBilbasen.DAL
+﻿using HtmlAgilityPack;
+using ScrapeBilbasen.Model;
+using ScrapySharp.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Http;
+using ScrapySharp.Network;
+using System.Text;
+
+namespace ScrapeBilbasen.DAL
 {
     public class DataAccess
     {
+        public static IConfiguration Configuration { get; private set; }
+        static string connectionString;
+        static ScrapingBrowser _scrapingBrowser = new ScrapingBrowser();
+        public DataAccess()
+        {
+            var build = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            Configuration = build.Build();
+            connectionString = Configuration.GetConnectionString("DefaultConnection");
+        }
+
+        #region Car 
+      
+        #endregion
     }
 }
